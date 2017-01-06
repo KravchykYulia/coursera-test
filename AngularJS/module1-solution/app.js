c482923cea8b71b215d202aa4fd6614ccae00b7f
KravchykYulia <julight22@gmail.com>
2017-01-06 09:56:36 +0200

    Assignment

--- AngularJS/module1-solution/app.js
+++ AngularJS/module1-solution/app.js
@@ -10,25 +10,27 @@ function LunchCheckController($scope) {
 
   $scope.countItems = function () {
     var totalItems=countItemsForString($scope.lunchList);
-    consol.log (totalItems);
+    console.log (totalItems);
     return totalItems;
   };
 
   function countItemsForString (str) {
    var totalCountItem=0;
    var stringArray=str.split(",");
+   /* I do NOT consider and empty item!!! Here is filter of 'empty' items */
    var correctStringArray=stringArray.filter(function(e){
     return e.search(/^\s*$/g)==-1;
-   });
+   });                                                    // end filter
    totalCountItem=correctStringArray.length;
    return totalCountItem;
-  };
+  };                                                      // end countItemsForString
   
-  $scope.displayMessage = function() {
-    if ($scope.countItems==0) {$scope.message="Please enter data first"}
-    	else if ($scope.countItems<=3) {$scope.message="Enjoy!"}
-    		else {$scope.message="Too much"}
-  };
+   $scope.displayMessage = function() {
+   	var condition=$scope.countItems();
+    if (condition==0) {$scope.message="Please enter data first"}
+     	else if (condition<=3) {$scope.message="Enjoy!"}
+     		else {$scope.message="Too much"}
+   };
 };
 
 })();
