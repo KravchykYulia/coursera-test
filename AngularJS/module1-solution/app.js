32394b5ab5fc01fc92c14430e720fa78bbf5ffd2
KravchykYulia <julight22@gmail.com>
2017-01-06 09:30:16 +0200

    Assignment

--- AngularJS/module1-solution/app.js
+++ AngularJS/module1-solution/app.js
@@ -4,14 +4,31 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope, $filter) {
-  $scope.name = "Yaakov";
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunchList = "";
 
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
+  $scope.countItems = function () {
+    var totalItems=countItemsForString($scope.lunchList);
+    consol.log (totalItems);
+    return totalItems;
   };
-}
+
+  function countItemsForString (str) {
+   var totalCountItem=0;
+   var stringArray=str.split(",");
+   var correctStringArray=stringArray.filter(function(e){
+    return e.search(/^\s*$/g)==-1;
+   });
+   totalCountItem=correctStringArray.length;
+   return totalCountItem;
+  };
+  
+  $scope.displayMessage = function() {
+    if ($scope.countItems==0) {$scope.message="Please enter data first"}
+    	else if ($scope.countItems<=3) {$scope.message="Enjoy!"}
+    		else {$scope.message="Too much"}
+  };
+};
 
 })();
