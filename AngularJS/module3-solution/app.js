bc62d166b0a8cb0cdc1a2cd04bbd2032495a7407
KravchykYulia <julight22@gmail.com>
2017-01-23 16:17:30 +0200

    Assignment

---
+++ AngularJS/module3-solution/app.js
@@ -0,0 +1,52 @@
+(function () {
+'use strict';
+
+ angular.module('NarrowItDownApp', [])
+ .controller('NarrowItDownController', NarrowItDownController)
+ .service('MenuSearchService', MenuSearchService)
+ .directive('foundItems', FoundItemsDirective);
+ 
+ // function FoundItemsDirective() {
+ //  var ddo = {
+ //    templateUrl: 'loader/itemsloaderindicator.template.html',
+ //    scope: {
+ //         items: '<',
+ //         onRemove: '&'
+ //    }
+ //  };
+ //  return ddo;
+ // };
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+ function MenuCategoriesController(MenuSearchService) {
+   var nidc = this;
+   nidc.searchTerm="";
+   nidc.found=function(){
+   MenuSearchService.getMatchedMenuItems(nidc.searchTerm);};
+   nidc.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);};
+ };
+
+ MenuSearchService.$inject =  ['$http'];
+ function MenuSearchService($http){ 
+     var service = this;
+     service.getMatchedMenuItems = function (searchTerm) {
+      return $http({
+       method: "GET",
+       url: ("https://davids-restaurant.herokuapp.com/menu_items.json") })
+      .then(function (result) {
+        // process result and only keep items that match
+        var foundItems=[];
+        var menuItems=result.data;
+        for (var i=0; i<menuItems.length; i++){
+          if(searchTerm=="" || menuItems[i].description.indexOf("searchTerm")==-1) {console.log("Nothing found");}
+          else {foundItems.push(menuItems[i]);}
+        }
+            // return processed items
+        return foundItems;
+      });}
+      service.removeItem = function (itemIndex) {
+         items.splice(itemIndex, 1);};
+ };
+
+})();
