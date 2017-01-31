778c45fac31df5c4a5cd972b52e0cdfed2bf19dc
KravchykYulia <julight22@gmail.com>
2017-01-31 09:47:49 +0200

    Assignment

--- AngularJS/module3-solution/app.js
+++ AngularJS/module3-solution/app.js
@@ -28,68 +28,70 @@
     };
 }
 
+NarrowItDownController.$inject = ['MenuSearchService'];
+ function NarrowItDownController(MenuSearchService) {
+   var list = this;
+ //  list.searchTerm="";
+   list.found= function(){
+   MenuSearchService.getMatchedMenuItems(list.searchTerm);
+   list.foundItems=foundItems;
+   //console.log (foundItems); //??????????
+   }; 
+   list.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);};
+   };
+
 // NarrowItDownController.$inject = ['MenuSearchService'];
 //  function NarrowItDownController(MenuSearchService) {
 //    var list = this;
+//    var promis=MenuSearchService.getMenuItems();
+//    promis.then(function(result) {
+//      list.menuItems=result.data.menu_items;
+//    });
+    
 //  //  list.searchTerm="";
 //    list.found= function(){
-//    MenuSearchService.getMatchedMenuItems(list.searchTerm);
+//    ;
 //   // console.log (MenuSearchService.foundItems); //??????????
 //    }; 
 //    list.removeItem = function (itemIndex) {
 //     MenuSearchService.removeItem(itemIndex);};
 //    };
 
-NarrowItDownController.$inject = ['MenuSearchService'];
- function NarrowItDownController(MenuSearchService) {
-   var list = this;
-   var promis=MenuSearchService.getMenuItems();
-   promis.then(function(result) {
-     list.menuItems=result.data.menu_items;
-   });
-    
- //  list.searchTerm="";
-   list.found= function(){
-   ;
-  // console.log (MenuSearchService.foundItems); //??????????
-   }; 
-   list.removeItem = function (itemIndex) {
-    MenuSearchService.removeItem(itemIndex);};
-   };
-
- // MenuSearchService.$inject =  ['$http'];
- // function MenuSearchService($http){ 
- //     var service = this;
- //     service.getMatchedMenuItems = function (searchTerm) {
- //       return $http({
- //       method: "GET",
- //       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")})
- //       .then(function (result) {
- //      // process result and only keep items that match
- //         var menuItems=result.data.menu_items;
- //         var foundItems=[];
- //         for (var i=0; i<menuItems.length; i++){
- //           if(searchTerm!=="" && menuItems[i].description.indexOf(searchTerm)!==-1) 
- //           {foundItems.push(menuItems[i]);}     
- //          }
- //        // return processed items
- //         console.log (foundItems);
- //         service.foundItems=foundItems
- //         return service.foundItems;
- //       });
- //    }; 
-    MenuSearchService.$inject =  ['$http'];
-    function MenuSearchService($http){ 
+ MenuSearchService.$inject =  ['$http'];
+ function MenuSearchService($http){ 
      var service = this;
-     service.getMenuItems = function () {
-      var result=$http({
+     service.getMatchedMenuItems = function (searchTerm) {
+       return $http({
        method: "GET",
-       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")});
-       return result;
-       };
-      service.getMatchedMenuItems = function (searchTerm) {
+       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")})
+       .then(function (result) {
+      // process result and only keep items that match
+         var menuItems=result.data.menu_items;
+         var foundItems=[];
+         for (var i=0; i<menuItems.length; i++){
+           if(searchTerm!=="" && menuItems[i].description.indexOf(searchTerm)!==-1) 
+           {foundItems.push(menuItems[i]);}     
+          }
+        // return processed items
+         console.log (foundItems);
+         // service.foundItems=foundItems;
+         // return service.foundItems;
+         return foundItems; 
+       });
+    }; 
+ //    MenuSearchService.$inject =  ['$http'];
+ //    function MenuSearchService($http){ 
+ //     var service = this;
+ //     service.getMenuItems = function () {
+ //      var result=$http({
+ //       method: "GET",
+ //       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")});
+ //       return result;
+ //       };
+ //      service.getMatchedMenuItems = function (searchTerm) {
         
-      };
+ //      };
       service.removeItem = function (itemIndex) {
          foundItems.splice(itemIndex, 1);};
  };
